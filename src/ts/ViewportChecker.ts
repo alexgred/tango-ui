4e2b77eb095920f1ee1376ac9f05e8a16365a748
Alex Dolzhenkov <alexgred.dev@gmail.com>
2018-09-06 13:56:32 +0300

    Change name and commenting code

--- src/ts/ViewportChecker.ts
+++ src/ts/ViewportChecker.ts
@@ -1,9 +1,15 @@
+/**
+ * Interface for options Object.
+ */
 interface IOptions {
   classToAdd: string;
   offset: number;
 }
 
-
+/**
+ * ViewportChecker class to check the scrolling position and 
+ * add the class to the element in the specified position.
+ */
 export default class ViewportChecker {
   public options: IOptions = {
     classToAdd: 'visible',
@@ -13,24 +19,42 @@ export default class ViewportChecker {
   private windowHeigth: number;
   public element: HTMLElement;
 
+  /**
+   * Constructor.
+   * 
+   * @param selector - Selector of element 
+   * @param userOptions - Object for user options
+   */
   constructor(selector: string, userOptions?: IOptions) {
     this.options = {...this.options, ...userOptions};
     this.windowHeigth = this.getWindowHeight();
     this.element = document.querySelector(selector);
 
+    // Event for scrolling.
     window.onscroll = () => {
       this.windowHeigth = this.getWindowHeight();
       this.checkElements(this.element);
     };
 
+    // Call the method by onload event.
     this.checkElements(this.element);
   }
 
+  /**
+   * Allows to get window height.
+   * 
+   * @return Window height
+   */
   private getWindowHeight(): number {
 
     return this.window.innerHeight;
   }
 
+  /**
+   * Main menthod with all logic.
+   * 
+   * @param element - Element to add class
+   */
   private checkElements(element: HTMLElement): void {
     const viewportStart: number = Math.max(
       document.body.scrollTop,
@@ -39,7 +63,7 @@ export default class ViewportChecker {
     );
     const viewportEnd: number = (viewportStart + this.windowHeigth);
     
-    const elementStart = Math.round(element.offsetTop) + this.options.offset;
+    const elementStart = Math.round(element.offsetTop) + this.options.offset; 
     const elementEnd = elementStart + element.clientHeight;
 
     if ((elementStart < viewportEnd) && (elementEnd > viewportStart)) {
